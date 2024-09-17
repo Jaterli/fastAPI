569b1986407af8df75411f0ae9e6e10526fcddff
jt <jaterli@hotmail.com>
2024-09-18 00:00:29 +0200

    Redimensionando

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def fetch_data():
         return {}
 
 # Función para generar la gráfica de barras apiladas
+@st.cache_data
 def plot_stacked_bar_chart(data):
     # Obtener todas las categorías y los meses
     categories = list(data.keys())
@@ -27,29 +28,30 @@ def plot_stacked_bar_chart(data):
     # Configurar los colores
     colors = ['skyblue', 'coral', 'yellowgreen', 'lightcoral', 'cyan']
 
-    fig, ax = plt.subplots(figsize=(4, 2))  # Ajustar el tamaño de la gráfica
+    fig, ax = plt.subplots(figsize=(2, 0.8))  # Ajustar el tamaño de la gráfica
 
     # Iterar por cada categoría para apilar las barras
     for i, category in enumerate(categories):
         counts = [data[category].get(month, 0) for month in months]  # Obtener los valores para cada mes
         p = ax.bar(months, counts, bottom=bottom, label=category, width=0.3, color=colors[i % len(colors)])  # Agregar la barra
         bottom += np.array(counts)  # Actualizar el bottom para la próxima barra
-        ax.bar_label(p, label_type='center')
+        ax.bar_label(p, label_type='center', fontsize=6)
+        
 
     # Configurar etiquetas y título
     # ax.set_xlabel('Mes', fontsize=12)
-    ax.set_ylabel('Número de Posts', fontsize=8)
-    ax.set_title('Número de Posts por Mes', fontsize=8)
+    ax.set_ylabel('Número de Posts', fontsize=7)
+    ax.set_title('Número de Posts por Mes', fontsize=7)
 
     # Configurar el tamaño de los ticks del eje X y Y
-    ax.tick_params(axis='x', labelsize=8)  # Tamaño de los valores en el eje X
-    ax.tick_params(axis='y', labelsize=8)  # Tamaño de los valores en el eje Y
+    ax.tick_params(axis='x', labelsize=7)  # Tamaño de los valores en el eje X
+    ax.tick_params(axis='y', labelsize=7)  # Tamaño de los valores en el eje Y
     # Mostrar la leyenda con un tamaño personalizado
 
     plt.xticks(rotation=45)
 
     # Mostrar la leyenda
-    ax.legend(fontsize=6.5)  # Tamaño de la leyenda
+    ax.legend(fontsize=6)  # Tamaño de la leyenda
 
 
     # Mostrar la gráfica en Streamlit
